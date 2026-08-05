b665accb157f7453968e378a2390fd54478c1376
JJiiyun <jiyunkim.gm@gmail.com>
2026-08-05 15:24:56 +0900

    refactor: OTA client 컨텍스트에 writer 통합

--- fhss-ota-radio/components/ota_client/source/ota_client_internal.h
+++ fhss-ota-radio/components/ota_client/source/ota_client_internal.h
@@ -1,18 +1,16 @@
 #pragma once
 
-#include <stdbool.h>
 #include <stdint.h>
-
 #include "freertos/FreeRTOS.h"
 
-#include "esp_ota_ops.h"
-#include "esp_partition.h"
-
 #include "ota_client.h"
+#include "ota_writer.h"
 
 typedef struct {
     ota_client_state_t state;
     ota_client_config_t config;
+
+    ota_writer_t writer;
 
     uint32_t session_id;
     uint32_t image_size;
@@ -20,12 +18,8 @@ typedef struct {
 
     uint32_t total_chunks;
     uint32_t expected_sequence;
-
     uint8_t expected_sha256[32];
-
-    const esp_partition_t *update_partition;
-    esp_ota_handle_t ota_handle;
-
     TickType_t last_packet_tick;
-    bool ota_started;
+
+
 } ota_client_context_t;
